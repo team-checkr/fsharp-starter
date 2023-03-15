06e082b36c39c7328ce2718ee7494ea0e054d010
Oliver Bøving <oliver@bvng.dk>
2023-03-15 10:42:06 +0100

    Rework the input/output of interpreter
    The changes reflect the comments from #4

--- Interpreter.fs
+++ Interpreter.fs
@@ -14,19 +14,39 @@ type InterpreterMemory =
 type Input =
     { determinism: Determinism
       assignment: InterpreterMemory
-      trace_size: int }
+      trace_length: int }
 
-type ProgramState =
+// TODO: Change this to you internal version of node
+// If your node type is defined in graph, consider using the following:
+// type Node = Graph.Node
+type Node = string
+
+type TerminationState =
     | Running
     | Stuck
     | Terminated
 
-type ProgramTrace =
-    { node: string
-      state: ProgramState
+type Configuration<'node> =
+    { node: 'node
       memory: InterpreterMemory }
 
-type Output = List<ProgramTrace>
+type Output =
+    { execution_sequence: List<Configuration<string>>
+      final: TerminationState }
+
+let stringifyNode (internalNode: Node) : string =
+    // TODO: implement for internal node type
+    internalNode
+
+let prepareConfiguration (c: Configuration<Node>) : Configuration<string> =
+    { node = stringifyNode c.node
+      memory = c.memory }
+
 
 let analysis (src: string) (input: Input) : Output =
     failwith "Interpreter not yet implemented" // TODO: start here
+    let execution_sequence: List<Configuration<Node>> = failwith "TODO"
+    let final: TerminationState = failwith "TODO"
+
+    { execution_sequence = List.map prepareConfiguration execution_sequence
+      final = final }
